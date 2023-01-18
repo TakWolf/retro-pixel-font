e97dc11320fe2747338684479ae84985dda5dfd7
TakWolf <takwolf@foxmail.com>
2023-01-18 15:07:32 +0800

    git 部署测试

---
+++ services/publish_service.py
@@ -0,0 +1,34 @@
+import datetime
+import logging
+import os
+import shutil
+
+import git
+
+import configs
+from configs import path_define
+from utils import fs_util
+
+logger = logging.getLogger('publish-service')
+
+
+def _copy_file(file_name, from_dir, to_dir):
+    from_path = os.path.join(from_dir, file_name)
+    to_path = os.path.join(to_dir, file_name)
+    shutil.copyfile(from_path, to_path)
+    logger.info(f'copy from {from_path} to {to_path}')
+
+
+def update_www():
+    fs_util.delete_dir(path_define.www_dir)
+    shutil.copytree(path_define.www_static_dir, path_define.www_dir)
+
+
+def deploy_www():
+    repo = git.Repo.init(path_define.www_dir)
+    repo.git.add(all=True)
+    repo.git.commit(m=f'deployed at {datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()}')
+    current_branch_name = repo.git.branch(show_current=True)
+    for git_deploy_config in configs.git_deploy_configs:
+        repo.git.remote('add', git_deploy_config.remote_name, git_deploy_config.url)
+        repo.git.push(git_deploy_config.remote_name, f'{current_branch_name}:{git_deploy_config.branch_name}', '-f')
---
+++ tasks/deploy_www.py
@@ -0,0 +1,14 @@
+import logging
+
+from services import publish_service
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    publish_service.update_www()
+    publish_service.deploy_www()
+
+
+if __name__ == '__main__':
+    main()
