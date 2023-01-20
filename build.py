2f7426bc97be19a319f81f67fff3322b615b8138
TakWolf <takwolf@foxmail.com>
2023-01-20 10:42:19 +0800

    自动生成许可证和字母表

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import logging
 
 import configs
 from configs import path_define
-from services import design_service, font_service
+from services import design_service, font_service, info_service
 from utils import fs_util
 
 logging.basicConfig(level=logging.DEBUG)
@@ -16,6 +16,8 @@ def main():
         design_service.verify_glyph_files(font_config)
         alphabet, glyph_file_paths = design_service.collect_glyph_files(font_config)
         font_service.make_fonts(font_config, alphabet, glyph_file_paths)
+        info_service.make_ofl_txt_file(font_config)
+        info_service.make_alphabet_txt_file(font_config, alphabet)
 
 
 if __name__ == '__main__':
---
+++ services/info_service.py
@@ -0,0 +1,35 @@
+import logging
+import os.path
+
+from configs import path_define
+from utils import fs_util
+
+logger = logging.getLogger('info-service')
+
+_ofl_string_format = None
+
+
+def _load_ofl_string_format():
+    global _ofl_string_format
+    if _ofl_string_format is None:
+        ofl_file_path = os.path.join(path_define.project_root_dir, 'LICENSE-OFL')
+        with open(ofl_file_path, 'r', encoding='utf-8') as file:
+            _ofl_string_format = file.read()
+    return _ofl_string_format
+
+
+def make_ofl_txt_file(font_config):
+    fs_util.make_dirs_if_not_exists(font_config.outputs_dir)
+    txt_file_path = os.path.join(font_config.outputs_dir, 'OFL.txt')
+    with open(txt_file_path, 'w', encoding='utf-8') as file:
+        ofl_string = _load_ofl_string_format().format(name=font_config.display_name)
+        file.write(ofl_string)
+    logger.info(f'make {txt_file_path}')
+
+
+def make_alphabet_txt_file(font_config, alphabet):
+    fs_util.make_dirs_if_not_exists(font_config.outputs_dir)
+    txt_file_path = os.path.join(font_config.outputs_dir, 'alphabet.txt')
+    with open(txt_file_path, 'w', encoding='utf-8') as file:
+        file.write(''.join(alphabet))
+    logger.info(f'make {txt_file_path}')
