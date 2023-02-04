1b4452cdf5e0bdeefd9e8d9d08527034ec7da9cc
TakWolf <takwolf@foxmail.com>
2023-02-04 21:27:08 +0800

    更改主页 HTML 主题色

--- services/html_service.py
+++ services/html_service.py
@@ -27,7 +27,7 @@ def make_alphabet_html_file(font_config, alphabet):
 def make_index_html_file():
     template = configs.template_env.get_template('index.html')
     html = template.render(font_configs=configs.font_configs)
-    # FIXME 'minify_css=True' 会造成背景动画闪烁，原因不明
+    # FIXME 这里样式压缩会造成背景动画闪烁
     html = minify_html.minify(html, minify_css=False, minify_js=True)
     fs_util.make_dirs_if_not_exists(path_define.outputs_dir)
     html_file_path = os.path.join(path_define.outputs_dir, 'index.html')
