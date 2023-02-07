b0e2cab2cfeed007f96658ef78b31002faca7ae0
TakWolf <takwolf@foxmail.com>
2023-02-07 13:27:57 +0800

    添加快速检索字体配置 map

--- configs/__init__.py
+++ configs/__init__.py
@@ -13,6 +13,7 @@ build_random_key = random.random()
 font_version = font_config.version
 
 font_configs = FontConfig.loads()
+font_config_map = {font_config.output_name: font_config for font_config in font_configs}
 
 font_formats = ['otf', 'woff2', 'ttf']
 
