e9aebbcf4ef407a5947dcec1bd85d50969c8b016
TakWolf <takwolf@foxmail.com>
2023-08-11 14:36:38 +0800

    代码整理

--- services/font_service.py
+++ services/font_service.py
@@ -17,45 +17,46 @@ def format_glyph_files(font_config: FontConfig):
     root_dir = os.path.join(path_define.glyphs_dir, font_config.outputs_name)
     tmp_dir = os.path.join(path_define.glyphs_tmp_dir, font_config.outputs_name)
     fs_util.delete_dir(tmp_dir)
-    for file_from_dir, file_name in fs_util.walk_files(root_dir):
-        file_from_path = os.path.join(file_from_dir, file_name)
+    for file_from_dir, _, file_names in os.walk(root_dir):
+        for file_name in file_names:
+            file_from_path = os.path.join(file_from_dir, file_name)
 
-        if file_name == 'config.toml':
-            file_to_dir = tmp_dir
+            if file_name == 'config.toml':
+                file_to_dir = tmp_dir
+                file_to_path = os.path.join(file_to_dir, file_name)
+                assert not os.path.exists(file_to_path), f"Config file duplication: '{file_from_path}'"
+                fs_util.make_dirs(file_to_dir)
+                shutil.copyfile(file_from_path, file_to_path)
+                logger.info("Copy config file: '%s'", file_to_path)
+                continue
+
+            if not file_name.endswith('.png'):
+                continue
+            if file_name == 'notdef.png':
+                file_to_dir = tmp_dir
+            else:
+                code_point = int(file_name.removesuffix('.png'), 16)
+                file_name = f'{code_point:04X}.png'
+                block = unidata_blocks.get_block_by_code_point(code_point)
+                block_dir_name = f'{block.code_start:04X}-{block.code_end:04X} {block.name}'
+                file_to_dir = os.path.join(tmp_dir, block_dir_name)
             file_to_path = os.path.join(file_to_dir, file_name)
-            assert not os.path.exists(file_to_path), f"Config file duplication: '{file_from_path}'"
+            assert not os.path.exists(file_to_path), f"Glyph file duplication: '{file_from_path}'"
+
+            glyph_data, glyph_width, glyph_height = glyph_util.load_glyph_data_from_png(file_from_path)
+            assert (glyph_height - font_config.size) % 2 == 0, f"Incorrect glyph data: '{file_from_path}'"
+            if glyph_height > font_config.line_height:
+                for i in range((glyph_height - font_config.line_height) // 2):
+                    glyph_data.pop(0)
+                    glyph_data.pop()
+            elif glyph_height < font_config.line_height:
+                for i in range((font_config.line_height - glyph_height) // 2):
+                    glyph_data.insert(0, [0 for _ in range(glyph_width)])
+                    glyph_data.append([0 for _ in range(glyph_width)])
+
             fs_util.make_dirs(file_to_dir)
-            shutil.copyfile(file_from_path, file_to_path)
-            logger.info("Copy config file: '%s'", file_to_path)
-            continue
-
-        if not file_name.endswith('.png'):
-            continue
-        if file_name == 'notdef.png':
-            file_to_dir = tmp_dir
-        else:
-            code_point = int(file_name.removesuffix('.png'), 16)
-            file_name = f'{code_point:04X}.png'
-            block = unidata_blocks.get_block_by_code_point(code_point)
-            block_dir_name = f'{block.code_start:04X}-{block.code_end:04X} {block.name}'
-            file_to_dir = os.path.join(tmp_dir, block_dir_name)
-        file_to_path = os.path.join(file_to_dir, file_name)
-        assert not os.path.exists(file_to_path), f"Glyph file duplication: '{file_from_path}'"
-
-        glyph_data, glyph_width, glyph_height = glyph_util.load_glyph_data_from_png(file_from_path)
-        assert (glyph_height - font_config.size) % 2 == 0, f"Incorrect glyph data: '{file_from_path}'"
-        if glyph_height > font_config.line_height:
-            for i in range((glyph_height - font_config.line_height) // 2):
-                glyph_data.pop(0)
-                glyph_data.pop()
-        elif glyph_height < font_config.line_height:
-            for i in range((font_config.line_height - glyph_height) // 2):
-                glyph_data.insert(0, [0 for _ in range(glyph_width)])
-                glyph_data.append([0 for _ in range(glyph_width)])
-
-        fs_util.make_dirs(file_to_dir)
-        glyph_util.save_glyph_data_to_png(glyph_data, file_to_path)
-        logger.info("Format glyph file: '%s'", file_to_path)
+            glyph_util.save_glyph_data_to_png(glyph_data, file_to_path)
+            logger.info("Format glyph file: '%s'", file_to_path)
     shutil.rmtree(root_dir)
     os.rename(tmp_dir, root_dir)
 
@@ -64,15 +65,16 @@ def collect_glyph_files(font_config: FontConfig) -> tuple[list[str], dict[int, s
     root_dir = os.path.join(path_define.glyphs_dir, font_config.outputs_name)
 
     registry = {}
-    for glyph_file_dir, glyph_file_name in fs_util.walk_files(root_dir):
-        if not glyph_file_name.endswith('.png'):
-            continue
-        glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
-        if glyph_file_name == 'notdef.png':
-            code_point = -1
-        else:
-            code_point = int(glyph_file_name.removesuffix('.png'), 16)
-        registry[code_point] = glyph_file_path
+    for glyph_file_dir, _, glyph_file_names in os.walk(root_dir):
+        for glyph_file_name in glyph_file_names:
+            if not glyph_file_name.endswith('.png'):
+                continue
+            glyph_file_path = os.path.join(glyph_file_dir, glyph_file_name)
+            if glyph_file_name == 'notdef.png':
+                code_point = -1
+            else:
+                code_point = int(glyph_file_name.removesuffix('.png'), 16)
+            registry[code_point] = glyph_file_path
 
     sequence = list(registry.keys())
     sequence.sort()
