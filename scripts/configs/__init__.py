d884c1c004fee4f42c56b6ee092ae4457a851680
TakWolf <takwolf@foxmail.com>
2024-03-17 13:19:32 +0800

    优化文件系统和 git 部署

--- scripts/configs/__init__.py
+++ scripts/configs/__init__.py
@@ -7,8 +7,8 @@ font_formats = ['otf', 'woff2', 'ttf', 'bdf']
 font_configs = FontConfig.loads()
 outputs_name_to_config = {font_config.outputs_name: font_config for font_config in font_configs}
 
-git_deploy_configs = [GitDeployConfig(
+git_deploy_config = GitDeployConfig(
     url='git@github.com:TakWolf/retro-pixel-font.git',
     remote_name='github',
     branch_name='gh-pages',
-)]
+)
--- scripts/services/publish_service.py
+++ scripts/services/publish_service.py
@@ -1,13 +1,12 @@
 import datetime
 import logging
 import os
-import shutil
 import zipfile
 
 import git
 
 from scripts import configs
-from scripts.configs import path_define, FontConfig
+from scripts.configs import path_define, FontConfig, GitDeployConfig
 from scripts.utils import fs_util
 
 logger = logging.getLogger('publish_service')
@@ -15,6 +14,7 @@ logger = logging.getLogger('publish_service')
 
 def make_release_zips():
     fs_util.make_dir(path_define.releases_dir)
+
     for font_format in configs.font_formats:
         file_path = os.path.join(path_define.releases_dir, f'{FontConfig.ZIP_OUTPUTS_NAME}-{font_format}-v{FontConfig.VERSION}.zip')
         with zipfile.ZipFile(file_path, 'w') as file:
@@ -26,43 +26,6 @@ def make_release_zips():
                 file.write(os.path.join(font_config.outputs_dir, 'info.txt'), os.path.join(font_config.outputs_name, 'info.txt'))
                 file.write(os.path.join(font_config.outputs_dir, 'preview.png'), os.path.join(font_config.outputs_name, 'preview.png'))
         logger.info("Make release zip: '%s'", file_path)
-
-
-def _copy_file(file_name: str, dir_from: str, dir_to: str):
-    path_from = os.path.join(dir_from, file_name)
-    path_to = os.path.join(dir_to, file_name)
-    shutil.copyfile(path_from, path_to)
-    logger.info("Copy from '%s' to '%s'", path_from, path_to)
-
-
-def update_docs():
-    fs_util.delete_dir(path_define.docs_dir)
-    os.mkdir(path_define.docs_dir)
-    _copy_file('readme-banner.png', path_define.outputs_dir, path_define.docs_dir)
-    for font_config in configs.font_configs:
-        fs_util.make_dir(font_config.docs_dir)
-        _copy_file('preview.png', font_config.outputs_dir, font_config.docs_dir)
-
-
-def update_www():
-    fs_util.delete_dir(path_define.www_dir)
-    shutil.copytree(path_define.www_static_dir, path_define.www_dir)
-    _copy_file('index.html', path_define.outputs_dir, path_define.www_dir)
-    for font_config in configs.font_configs:
-        fs_util.make_dir(font_config.www_dir)
-        _copy_file(f'{font_config.full_outputs_name}.woff2', font_config.outputs_dir, font_config.www_dir)
-        _copy_file('alphabet.html', font_config.outputs_dir, font_config.www_dir)
-        _copy_file('demo.html', font_config.outputs_dir, font_config.www_dir)
-
-
-def deploy_www():
-    repo = git.Repo.init(path_define.www_dir)
-    repo.git.add(all=True)
-    repo.git.commit(m=f'deployed at {datetime.datetime.now(datetime.UTC).isoformat()}')
-    current_branch_name = repo.git.branch(show_current=True)
-    for git_deploy_config in configs.git_deploy_configs:
-        repo.git.remote('add', git_deploy_config.remote_name, git_deploy_config.url)
-        repo.git.push(git_deploy_config.remote_name, f'{current_branch_name}:{git_deploy_config.branch_name}', '-f')
 
 
 def update_readme_md():
@@ -102,3 +65,49 @@ def update_readme_md():
 
     fs_util.write_str('\n'.join(front_lines + preview_lines + back_lines), file_path)
     logger.info("Update: 'README.md'")
+
+
+def update_docs():
+    fs_util.delete_dir(path_define.docs_dir)
+    fs_util.make_dir(path_define.docs_dir)
+
+    fs_util.copy_the_file('readme-banner.png', path_define.outputs_dir, path_define.docs_dir)
+    for font_config in configs.font_configs:
+        fs_util.make_dir(font_config.docs_dir)
+        fs_util.copy_the_file('preview.png', font_config.outputs_dir, font_config.docs_dir)
+
+
+def update_www():
+    fs_util.make_dir(path_define.www_dir)
+    for name in os.listdir(path_define.www_dir):
+        if name == '.git':
+            continue
+        fs_util.delete_item(os.path.join(path_define.www_dir, name))
+
+    for name in os.listdir(path_define.www_static_dir):
+        fs_util.copy_the_item(name, path_define.www_static_dir, path_define.www_dir)
+
+    fs_util.copy_the_file('index.html', path_define.outputs_dir, path_define.www_dir)
+    for font_config in configs.font_configs:
+        fs_util.make_dir(font_config.www_dir)
+        fs_util.copy_the_file(f'{font_config.full_outputs_name}.woff2', font_config.outputs_dir, font_config.www_dir)
+        fs_util.copy_the_file('alphabet.html', font_config.outputs_dir, font_config.www_dir)
+        fs_util.copy_the_file('demo.html', font_config.outputs_dir, font_config.www_dir)
+
+
+def deploy_www(config: GitDeployConfig):
+    if os.path.exists(os.path.join(path_define.www_dir, '.git')):
+        repo = git.Repo(path_define.www_dir)
+    else:
+        repo = git.Repo.init(path_define.www_dir)
+
+    if config.remote_name in repo.git.remote().splitlines():
+        repo.git.remote('rm', config.remote_name)
+    repo.git.remote('add', config.remote_name, config.url)
+
+    if len(repo.git.status('-s').splitlines()) > 0:
+        repo.git.add(all=True)
+        repo.git.commit(m=f'deployed at {datetime.datetime.now(datetime.UTC).isoformat()}')
+
+    current_branch_name = repo.git.branch(show_current=True)
+    repo.git.push(config.remote_name, f'{current_branch_name}:{config.branch_name}', '-f')
