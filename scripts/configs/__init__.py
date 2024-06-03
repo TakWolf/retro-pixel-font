edd1a7bc20a8c3a1927058201652e18f3fb19dbd
TakWolf <takwolf@foxmail.com>
2024-06-03 10:59:54 +0800

    优化 Demo 页面 git 部署逻辑

--- scripts/configs/__init__.py
+++ scripts/configs/__init__.py
@@ -10,8 +10,8 @@ font_version_time = datetime.datetime.fromisoformat(f'{font_version.replace('.',
 
 font_formats = ['otf', 'woff2', 'ttf', 'bdf', 'pcf']
 
-git_deploy_config = GitDeployConfig(
+git_deploy_configs = [GitDeployConfig(
     url='git@github.com:TakWolf/retro-pixel-font.git',
     remote_name='github',
     branch_name='gh-pages',
-)
+)]
