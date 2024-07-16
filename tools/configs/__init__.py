f33a700d1a8e2642308aea580088bd92fe497ebf
TakWolf <takwolf@foxmail.com>
2024-07-16 23:07:38 +0800

    调整 font_formats 配置顺序

--- tools/configs/__init__.py
+++ tools/configs/__init__.py
@@ -2,5 +2,5 @@ from typing import Literal, get_args
 
 version = '2024.05.12'
 
-type FontFormat = Literal['otf', 'woff2', 'ttf', 'bdf', 'pcf']
+type FontFormat = Literal['otf', 'ttf', 'woff2', 'bdf', 'pcf']
 font_formats = list[FontFormat](get_args(FontFormat.__value__))
