0eb3d9af8860d9766a38e72f1ed36884d849d959
TakWolf <takwolf@foxmail.com>
2025-06-19 16:51:14 +0800

    优化格式化工具

--- tools/services/format_service.py
+++ tools/services/format_service.py
@@ -1,23 +1,8 @@
-import shutil
-from pathlib import Path
-
 from pixel_font_knife import glyph_file_util
 
 from tools.configs.font import FontConfig
 
 
-def _is_empty_dir(path: Path) -> bool:
-    for item_path in path.iterdir():
-        if item_path.name == '.DS_Store':
-            continue
-        return False
-    return True
-
-
 def format_glyphs(font_config: FontConfig):
     context = glyph_file_util.load_context(font_config.glyphs_dir)
     glyph_file_util.normalize_context(context, font_config.glyphs_dir)
-
-    for file_dir, _, _ in font_config.glyphs_dir.walk(top_down=False):
-        if _is_empty_dir(file_dir):
-            shutil.rmtree(file_dir)
