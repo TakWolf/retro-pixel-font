b3366205c0ee208ce49af2f7d1fb258bc23aff87
TakWolf <takwolf@foxmail.com>
2025-07-02 10:49:53 +0800

    调整异常日志

--- tools/services/check_service.py
+++ tools/services/check_service.py
@@ -7,4 +7,4 @@ def check_glyph_files(font_config: FontConfig):
     context = glyph_file_util.load_context(font_config.glyphs_dir)
     for code_point, flavor_group in context.items():
         for glyph_file in set(flavor_group.values()):
-            assert glyph_file.height == font_config.line_height, f"[{font_config.outputs_name}] glyph bitmap error: '{glyph_file.file_path}'"
+            assert glyph_file.height == font_config.line_height, f"[{font_config.outputs_name}] glyph bitmap size error: '{glyph_file.file_path}'"
