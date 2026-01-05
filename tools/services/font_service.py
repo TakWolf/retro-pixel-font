1b5c3f4ebf02d51b2d406a463cc18395053c3c36
TakWolf <takwolf@foxmail.com>
2026-01-05 10:41:37 +0800

    使用匹配模式

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -84,14 +84,15 @@ def make_fonts(font_config: FontConfig, glyph_sequence: list[GlyphFile], charact
     builder = _create_builder(font_config, glyph_sequence, character_mapping)
     for font_format in options.font_formats:
         file_path = font_config.outputs_dir.joinpath(f'retro-pixel-{font_config.outputs_name}.{font_format}')
-        if font_format == 'otf.woff':
-            builder.save_otf(file_path, flavor=opentype.Flavor.WOFF)
-        elif font_format == 'otf.woff2':
-            builder.save_otf(file_path, flavor=opentype.Flavor.WOFF2)
-        elif font_format == 'ttf.woff':
-            builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF)
-        elif font_format == 'ttf.woff2':
-            builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF2)
-        else:
-            getattr(builder, f'save_{font_format}')(file_path)
+        match font_format:
+            case 'otf.woff':
+                builder.save_otf(file_path, flavor=opentype.Flavor.WOFF)
+            case 'otf.woff2':
+                builder.save_otf(file_path, flavor=opentype.Flavor.WOFF2)
+            case 'ttf.woff':
+                builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF)
+            case 'ttf.woff2':
+                builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF2)
+            case _:
+                getattr(builder, f'save_{font_format}')(file_path)
         logger.info("Make font: '{}'", file_path)
