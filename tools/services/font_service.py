9766e081c876cf2e68c168a3ff60adaa91281bc0
TakWolf <takwolf@foxmail.com>
2026-08-11 18:18:28 +0800

    简化格式导出方法调用

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -2,7 +2,7 @@ import math
 from datetime import datetime
 
 from loguru import logger
-from pixel_font_builder import FontBuilder, Glyph, opentype
+from pixel_font_builder import FontBuilder, Glyph
 from pixel_font_knife import glyph_file_util
 from pixel_font_knife.glyph_file_util import GlyphFile
 
@@ -84,15 +84,5 @@ def make_fonts(font_config: FontConfig, glyph_sequence: list[GlyphFile], charact
     builder = _create_builder(font_config, glyph_sequence, character_mapping)
     for font_format in options.font_formats:
         file_path = font_config.outputs_dir.joinpath(f'retro-pixel-{font_config.outputs_name}.{font_format}')
-        match font_format:
-            case 'otf.woff':
-                builder.save_otf(file_path, flavor=opentype.Flavor.WOFF)
-            case 'otf.woff2':
-                builder.save_otf(file_path, flavor=opentype.Flavor.WOFF2)
-            case 'ttf.woff':
-                builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF)
-            case 'ttf.woff2':
-                builder.save_ttf(file_path, flavor=opentype.Flavor.WOFF2)
-            case _:
-                getattr(builder, f'save_{font_format.replace('.', '_')}')(file_path)
+        getattr(builder, f'save_{font_format.replace('.', '_')}')(file_path)
         logger.info("Make font: '{}'", file_path)
