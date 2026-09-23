5eb0c4c86179998155a3f45810cafdbe5490954c
TakWolf <takwolf@foxmail.com>
2026-09-23 15:19:21 +0800

    调整校验文案

--- tools/services/check_service.py
+++ tools/services/check_service.py
@@ -7,4 +7,4 @@ def check_cmap_glyphs(font_config: FontConfig) -> None:
     context = CmapContext.load(font_config.glyphs_dir.joinpath('cmap'))
     for code_point, glyph_variants in context.items():
         for glyph_file in set(glyph_variants.values()):
-            assert glyph_file.canvas.height == font_config.line_height, f"[{font_config.outputs_name}] glyph bitmap size error: '{glyph_file.file_path}'"
+            assert glyph_file.canvas.height == font_config.line_height, f"[{font_config.outputs_name}] glyph bitmap dimensions error: '{glyph_file.file_path}'"
